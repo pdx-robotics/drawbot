d722df46ed90ed01716804faf37418d76bef6339
Geoffrey <geoolson@pdx.edu>
2019-04-09 08:25:56 -0700

    added a list data structure for storing the drawing

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const path = require('path');
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.get('/list.js', (req, res) => res.sendFile(path.join(__dirname + '/list.js')));
 app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
