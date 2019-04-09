dc4540f09b9e77df65bf608e28ef0c96d632b7c5
Geoffrey <geoolson@pdx.edu>
2019-04-08 17:55:07 -0700

    added a header with the logo.
    I still need to add the title and shift the logo to the left like the
    website.

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const port = 3000;
 const path = require('path');
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
