f97e018eb4137c79ad8c644b4378235865cbaedf
Geoffrey <geoolson@pdx.edu>
2019-04-09 14:34:33 -0700

    workinprogress: submitting json files to the server

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const formidable = require('formidable');
+const bodyParser = require('body-parser');
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 
 app.post('/test', function(req,res){
   console.log('received object');
   console.log(req.body);
-  console.log(req.params);
   res.end();
 });
 
--- public/submit.js
+++ public/submit.js
@@ -1,6 +1,8 @@
-function post(blob) {
+function post() {
   var xhttp = new XMLHttpRequest();
+  var string = JSON.stringify( {foo : "bar"} );
   xhttp.open("POST", "/test", true);
-  xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhttp.send("foo=bar&lorem=ipsum");
+  xhttp.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+  xhttp.send(string);
+  console.log(string);
 }
