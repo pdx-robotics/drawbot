b27d2375c8f6c1f3ea80ab77880920c7a0a89fb2
geoolson <geoolson@pdx.edu>
2019-04-09 13:33:13 -0700

    saving work

--- public/submit.js
+++ public/submit.js
@@ -1,5 +1,6 @@
 function post(blob) {
   var xhttp = new XMLHttpRequest();
   xhttp.open("POST", "/test", true);
+  xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   xhttp.send("foo=bar&lorem=ipsum");
 }
