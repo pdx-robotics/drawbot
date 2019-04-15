f0b04cd64ca22c15a29593433b8a85cffbc2457a
Geoffrey <geoolson@pdx.edu>
2019-04-15 04:24:35 +0100

    removed an unnecessary module

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+//const server = require('http').Server(app);
+//const io = require('socket.io')(server); // module for web socket.
 const port = 3000;
-const path = require('path');
 const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({
@@ -18,6 +19,16 @@ app.post('/test', function(req,res){
   res.end();
 });
 
+/*
+io.sockets.on('connection', function(socket){
+  var lightValue = 0;
+  socket.on('light', function(data) {
+    lightValue = data
+    if(lightValue)
+      console.log(lightValue);
+  });
+});
+*/
 app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Drawbot listening on port ${port}!`));
