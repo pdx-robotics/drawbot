170ea64d5d157bd98bc7b552c90289959e1e2928
Geoffrey <geoolson@pdx.edu>
2019-04-22 21:49:53 +0100

    some re-structuring of the code base
    there was some javascript and css inline in the index.html document. The css has been moved to style.css and the javscript code has been moved to main.js

--- public/main.js
+++ public/main.js
@@ -32,6 +32,48 @@ function submit(){
     post(doodle);
 }
 
+// start of modal code for the controller configuration
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// end of modal code
+
+
+// code for the check box
+var socket = io();
+window.addEventListener("load", function(){
+    var lightbox = document.getElementById("light");
+    lightbox.addEventListener("change", function() { 
+        socket.emit("light", Number(this.checked));
+        });
+    });
+// end of check box code
+
+
+
 function setrtc(){
     if (rtControl == false){
         rtControl = true;
